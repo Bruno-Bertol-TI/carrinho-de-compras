6b1caf59e238ec254bf49eec6e2b7921a725d7d3
Bruno Bertol <bbertol2312@outlook.com>
2025-04-11 17:23:03 -0300

    adicionando funcao para limpar carrinho

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 let totalCarrinho = 0;
+let listaCarrinho;
+let exibirTotal;
 
 function adicionar() {
     let produto = document.getElementById('produto').value;
     let qtdProduct = document.getElementById('quantidade').value;
-    
+
     let nameProduct = produto.split('-')[0]; // usando SPLIT
     let price = produto.split('R$')[1];
     let precoTotalItens = qtdProduct * price;
@@ -11,12 +13,20 @@ function adicionar() {
     if (qtdProduct > 0) {
         totalCarrinho = totalCarrinho +  precoTotalItens;
 
-        let listaCarrinho = document.getElementById('lista-produtos');
+        listaCarrinho = document.getElementById('lista-produtos');
         listaCarrinho.innerHTML = listaCarrinho.innerHTML + `<section class="carrinho__produtos__produto">
             <span class="texto-azul"> ${qtdProduct}x </span> ${nameProduct} <span class="texto-azul">R$ ${precoTotalItens}</span>
-            </section>`
+            </section>`;
         
-        let exibirTotal = document.getElementById('valor-total');
-        exibirTotal.innerHTML = `R$ ${totalCarrinho}`;
+        exibirTotal = document.getElementById('valor-total');
+        exibirTotal.innerHTML = `R$ ${totalCarrinho.toLocaleString('pt-BR', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2 
+         })}`;
     }
+}
+
+function limpar() {
+    listaCarrinho.innerHTML = ``;
+    exibirTotal.innerHTML = ``;
 }
